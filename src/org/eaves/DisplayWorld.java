54442f1c014a628153f7935380276f55eea37a6c
Jon Eaves <jon.eaves@rea-group.com>
2011-02-11 12:40:08 +1100

    Initial Version

---
+++ src/org/eaves/DisplayWorld.java
@@ -0,0 +1,8 @@
+package org.eaves;
+
+public class DisplayWorld {
+	public static void main(String args[])
+	{
+		System.out.println(new HelloWorld().greeting());
+	}
+}
---
+++ src/org/eaves/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.eaves;
+
+public class HelloWorld {
+
+	public String greeting()
+	{
+		return "Hello, World";
+	}
+
+}
