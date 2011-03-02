968c71b1ffee3e5150f2dd4d4b325feed195c765
Jon Eaves <jon.eaves@rea-group.com>
2011-03-02 14:08:52 +1100

    Started on the robot problem

---
+++ test/org/eaves/robot/TestRobot.java
@@ -0,0 +1,50 @@
+package org.eaves.robot;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class TestRobot {
+
+	private Robot _robot;
+	private Observer _comparable;
+
+	@Before
+	public void setUp() throws Exception {
+		TableTop table = new ClearTableTop(5,5);
+		Engine engine = new SingleStepEngine();
+		_robot = new NoFallRobot(engine, table);
+		
+		_comparable = new LocationOuputObserver(_robot);
+		
+		_robot.place(0,0, Direction.fromString("N"));
+	}
+	
+	@Test
+	public void testCreateEnvironment()
+	{
+		assertEquals(_comparable.getCurrentLocation(), new Location(0,0, Direction.fromString("N")));
+	}
+	
+	@Test
+	public void testMove() {
+		_robot.move();
+		assertEquals(_comparable.getCurrentLocation(), new Location(0,1, Direction.fromString("N")));
+	}
+	
+	@Test
+	public void testTurnLeft() {
+		_robot.left();
+		assertEquals(_comparable.getCurrentLocation(), new Location(0,0, Direction.fromString("W")));
+	}
+	
+	@Test
+	public void testTurnRight() {
+		_robot.right();
+		assertEquals(_comparable.getCurrentLocation(), new Location(0,1, Direction.fromString("E")));
+	}
+}
